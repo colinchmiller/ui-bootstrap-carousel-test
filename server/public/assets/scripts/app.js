c1e282a08e08ae6b96e7353b0d9192be12f5a9da
colinmiller <colinchmiller@gmail.com>
2016-03-10 14:54:59 -0600

    basic carousel elements, but no carousel function

--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -1,5 +1,16 @@
-var mainApp = angular.module('mainApp', []);
+var mainApp = angular.module('mainApp', ['ui.bootstrap.carousel']);
 
 mainApp.controller('TestController', function(){
   console.log("TestController firing!");
-})
+});
+
+mainApp.controller('ProtoCarousel', ['$scope', function($scope){
+  console.log("ProtoCarousel running!");
+  $scope.name = "Colin Miller"
+  $scope.myInterval = 3000;
+  $scope.slides = [
+    {image: "https://www.fillmurray.com/200/300"},
+    {image: "https://www.fillmurray.com/200/400"},
+    {image: "https://www.fillmurray.com/300/300"}
+  ];
+}]);
