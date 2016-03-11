e512171e9d2d96f2271cbe8c34a4d2df385533df
colinmiller <colinchmiller@gmail.com>
2016-03-11 10:51:29 -0600

    stuck after documentation for ui.bootstrap reference. Images not loading in as slides

--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -1,4 +1,4 @@
-var mainApp = angular.module('mainApp', ['ui.bootstrap.carousel']);
+var mainApp = angular.module('mainApp', ['ui.bootstrap']);
 
 mainApp.controller('TestController', function(){
   console.log("TestController firing!");
@@ -6,11 +6,11 @@ mainApp.controller('TestController', function(){
 
 mainApp.controller('ProtoCarousel', ['$scope', function($scope){
   console.log("ProtoCarousel running!");
-  $scope.name = "Colin Miller"
   $scope.myInterval = 3000;
-  $scope.slides = [
-    {image: "https://www.fillmurray.com/200/300"},
-    {image: "https://www.fillmurray.com/200/400"},
-    {image: "https://www.fillmurray.com/300/300"}
-  ];
+  $scope.active = 0;
+  $scope.slides = [{image: "https://www.fillmurray.com/200/300"},
+  {image: "https://www.fillmurray.com/200/400"},
+  {image: "https://www.fillmurray.com/300/300"}];
+  var currIndex = 0;
+
 }]);
