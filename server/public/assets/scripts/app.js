ac44d7f69f28df16486f5336759dc0c6fd60528c
colinmiller <colinchmiller@gmail.com>
2016-03-18 08:59:09 -0500

    add caption to slides, minor styling

--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -2,12 +2,12 @@ var mainApp = angular.module('mainApp', ['ngAnimate','ui.bootstrap']);
 
 angular.module('mainApp').controller('ProtoCarousel', ['$scope', function($scope){
   console.log("ProtoCarousel running!");
-  $scope.myInterval = 3000;
+  $scope.myInterval = 5000;
   $scope.noWrapSlides = false;
   $scope.active = 0;
-  $scope.slides = [{image: "https://www.fillmurray.com/800/300", id: 0},
-  {image: "https://www.fillmurray.com/900/400", id: 1},
-  {image: "https://www.fillmurray.com/700/300", id: 2}];
+  $scope.slides = [{image: "https://www.fillmurray.com/800/300", text: "First Murray", id: 0},
+  {image: "https://www.fillmurray.com/500/400", text: "Second Murray", id: 1},
+  {image: "https://www.fillmurray.com/700/600", text: "Third Murray", id: 2}];
   var currIndex = 0;
 
 }]);
